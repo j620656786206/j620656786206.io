5bb6268e354a84d5029716248980142dfb84f793
j620656786206 <j620656786206@gmail.com>
2019-08-30 00:33:00 +0800

    Update components

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,33 +7,69 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
+import { Link } from "gatsby";
 
-import Header from "./header";
-import "./layout.css";
+import { rhythm, scale } from "../utils/typography";
 
-const Layout = ({ children }) => {
-	const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
+class Layout extends React.Component {
+	render() {
+		const { location, title, children } = this.props;
+		const rootPath = `j620656786206.io/`;
 
-	return (
-		<>
-			<Header siteTitle={data.site.siteMetadata.title} />
+		let header;
+
+		if (location.pathname === rootPath) {
+			header = (
+				<h1
+					style={{
+						...scale(1.5),
+						marginBottom: rhythm(1.5),
+						marginTop: 0,
+					}}
+				>
+					<Link
+						style={{
+							boxShadow: `none`,
+							textDecoration: `none`,
+							color: `inherit`,
+						}}
+						to={`/`}
+					>
+						{title}
+					</Link>
+				</h1>
+			);
+		} else {
+			header = (
+				<h3
+					style={{
+						fontFamily: `Montserrat, sans-serif`,
+						marginTop: 0,
+					}}
+				>
+					<Link
+						style={{
+							boxShadow: `none`,
+							textDecoration: `none`,
+							color: `inherit`,
+						}}
+						to={`/`}
+					>
+						{title}
+					</Link>
+				</h3>
+			);
+		}
+		return (
 			<div
 				style={{
-					margin: `0 auto`,
-					maxWidth: 960,
-					padding: `0px 1.0875rem 1.45rem`,
-					paddingTop: 0,
+					marginLeft: `auto`,
+					marginRight: `auto`,
+					maxWidth: rhythm(24),
+					padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
 				}}
 			>
+				<header>{header}</header>
 				<main>{children}</main>
 				<footer>
 					© {new Date().getFullYear()}, Built with
@@ -41,9 +77,9 @@ const Layout = ({ children }) => {
 					<a href="https://www.gatsbyjs.org">Gatsby</a>
 				</footer>
 			</div>
-		</>
-	);
-};
+		);
+	}
+}
 
 Layout.propTypes = {
 	children: PropTypes.node.isRequired,
